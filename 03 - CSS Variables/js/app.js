16bca74d198f725b19c6c9f4d728049721b8fcd6
Nəzrin <isbndyrva@gmail.com>
2021-11-30 22:41:17 +0400

    Add playing with CSS variables and JS project

---
+++ 03 - CSS Variables/js/app.js
@@ -0,0 +1,12 @@
+const inputs = document.querySelectorAll("input");
+
+function changeValue() {
+  const suffix = this.dataset.size || " ";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
+
+inputs.forEach((input) => input.addEventListener("change", changeValue));
+inputs.forEach((input) => input.addEventListener("mouseover", changeValue));
