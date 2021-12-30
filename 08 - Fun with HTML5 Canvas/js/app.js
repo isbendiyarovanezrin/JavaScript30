e10e1cc5ff85ce6370cb09ca4a2232c3698118e7
Nəzrin <isbndyrva@gmail.com>
2021-12-30 23:15:46 +0400

    Add fun with HTML5 canvas

---
+++ 08 - Fun with HTML5 Canvas/js/app.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const coordinate = { x: 0, y: 0 };
+
+document.addEventListener("mousedown", start);
+document.addEventListener("mouseup", stop);
+window.addEventListener("resize", resize);
+
+resize();
+function resize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+function getPosition(e) {
+  coordinate.x = e.clientX - canvas.offsetLeft;
+  coordinate.y = e.clientY - canvas.offsetTop;
+}
+
+function start(e) {
+  getPosition(e);
+  document.addEventListener("mousemove", draw);
+}
+
+function stop() {
+  document.removeEventListener("mousemove", draw);
+}
+
+let hue = 0;
+let lineWidth = 120;
+let direction = true;
+
+function draw(e) {
+  ctx.beginPath();
+  ctx.lineCap = "round";
+  ctx.lineWidth = `${lineWidth}`;
+  ctx.strokeStyle = `hsl(${hue}, 60%, 55%)`;
+  ctx.globalCompositeOperation = "xor";
+  ctx.moveTo(coordinate.x, coordinate.y);
+  getPosition(e);
+  ctx.lineTo(coordinate.x, coordinate.y);
+  ctx.stroke();
+
+  hue++;
+  if (360 <= hue) {
+    hue = 0;
+  }
+
+  if (ctx.lineWidth == 120 || ctx.lineWidth == 3) {
+    direction = !direction;
+  }
+
+  if (direction) {
+    lineWidth++;
+  } else {
+    lineWidth--;
+  }
+
+  ctx.closePath();
+}
