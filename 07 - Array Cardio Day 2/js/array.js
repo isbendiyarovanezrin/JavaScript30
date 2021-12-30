bc8228a17f523412659010bd5bd875ee9561a80b
Nəzrin <isbndyrva@gmail.com>
2021-12-30 06:41:08 +0400

    Add array cardio day 2

---
+++ 07 - Array Cardio Day 2/js/array.js
@@ -0,0 +1,42 @@
+// Array Cardio Day 2
+
+const people = [
+  { name: "Wes", year: 1988 },
+  { name: "Kait", year: 1986 },
+  { name: "Irv", year: 1970 },
+  { name: "Lux", year: 2015 },
+];
+
+const comments = [
+  { text: "Love this!", id: 523423 },
+  { text: "Super good", id: 823423 },
+  { text: "You are the best", id: 2039842 },
+  { text: "Ramen is my fav food ever", id: 123523 },
+  { text: "Nice Nice Nice!", id: 542328 },
+];
+
+// Some and Every Checks
+var today = new Date();
+var currentYear = today.getFullYear();
+
+// Array.prototype.some() // is at least one person 19 or older?
+let a = people.some((person) => 19 <= currentYear - person.year);
+console.log(a);
+
+// Array.prototype.every() // is everyone 19 or older?
+let b = people.every((person) => 19 <= currentYear - person.year);
+console.log(b);
+
+// Array.prototype.find()
+// find the comment with the ID of 823423
+let comment = comments.find((comment) => comment.id === 823423);
+console.log(comment);
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+let index = comments.findIndex((comment) => comment.id === 823423);
+console.log(index);
+
+// delete the comment with the ID of 823423
+comments.splice(index, 1);
+console.log(comments);
