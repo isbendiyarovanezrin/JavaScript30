336835044d4ceadd28cfabfb20ba1e04b06d686c
Nəzrin <isbndyrva@gmail.com>
2021-12-31 19:06:44 +0400

    Update array cardio day 1

--- 04 - Array Cardio Day 1/js/array.js
+++ 04 - Array Cardio Day 1/js/array.js
@@ -1,6 +1,21 @@
 // Array Cardio Day 1
 // Get your shorts on - this is an array workout!
 
+const inventors = [
+  { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
+  { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
+  { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
+  { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
+  { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
+  { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
+  { first: "Max", last: "Planck", year: 1858, passed: 1947 },
+  { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 },
+  { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 },
+  { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 },
+  { first: "Lise", last: "Meitner", year: 1878, passed: 1968 },
+  { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 },
+];
+
 const people = [
   "Bernhard, Sandra",
   "Bethea, Erin",
@@ -63,20 +78,6 @@ const people = [
 // const c = inventors.sort((i, l) => i.year - l.year);
 // console.table(c);
 
-const inventors = [
-  { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
-  { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
-  { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
-  { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
-  { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
-  { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
-  { first: "Max", last: "Planck", year: 1858, passed: 1947 },
-  { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 },
-  { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 },
-  { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 },
-  { first: "Lise", last: "Meitner", year: 1878, passed: 1968 },
-  { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 },
-];
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
 // let d = inventors.reduce(function (acc, inventor) {
