5e1e4504dfd361cb4db24fe03a4bc1db9c09386a
Nəzrin <isbndyrva@gmail.com>
2022-01-02 22:59:07 +0400

    Add click and drag project

---
+++ 27 - Click and Drag/js/app.js
@@ -0,0 +1,30 @@
+const slider = document.querySelector(".items");
+
+let isDown = false,
+  scrollLeft,
+  startX;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 4;
+  slider.scrollLeft = scrollLeft - walk;
+});
