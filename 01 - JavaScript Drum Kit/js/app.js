0fbdaac72e3a01842111fa30c9023227a773e763
Nəzrin <isbndyrva@gmail.com>
2022-01-07 22:29:13 +0400

    Update JavaScript drum kit

--- 01 - JavaScript Drum Kit/js/app.js
+++ 01 - JavaScript Drum Kit/js/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 window.addEventListener("keydown", function (i) {
   const key = document.querySelector(`.key[data-key="${i.key}"]`);
   const audio = document.querySelector(`audio[data-key="${i.key}"]`);
@@ -12,4 +14,17 @@ keys.forEach((key) => key.addEventListener("transitionend", removeStyle));
 
 function removeStyle() {
   this.classList.remove("playing");
+}
+
+// cursor
+const cursor = document.getElementById("cursor");
+
+document.addEventListener("mousemove", changeCursor);
+
+function changeCursor(e) {
+  const x = e.pageX;
+  const y = e.pageY;
+
+  cursor.style.left = `${x}px`;
+  cursor.style.top = `${y}px`;
 }
