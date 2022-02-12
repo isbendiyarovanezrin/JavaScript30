cc59540d1aa62bacd74a1795b93d5110fedebc87
Nəzrin <isbndyrva@gmail.com>
2022-02-12 05:34:19 +0400

    Add countdown timer project

---
+++ 29 - Countdown Timer/js/script.js
@@ -0,0 +1,90 @@
+const buttons = document.querySelectorAll("[data-seconds]");
+const form = document.querySelector("#form");
+const input = document.querySelector("#input");
+const remainingTime = document.querySelector(".display-remaining-time");
+const endTime = document.querySelector(".display-end-time");
+let countdown;
+
+function timer(seconds) {
+  clearInterval(countdown);
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayRemainingTime(seconds);
+  displayEndTime(then);
+  countdown = setInterval(() => {
+    const remainingSeconds = Math.round((then - Date.now()) / 1000);
+    if (remainingSeconds < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    displayRemainingTime(remainingSeconds);
+  });
+}
+
+function displayRemainingTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  const time = `${minutes < 10 ? "0" : ""}${minutes}:${
+    remainingSeconds < 10 ? "0" : ""
+  }${remainingSeconds}`;
+  remainingTime.textContent = time;
+  remainingTime.style.fontSize = "22rem";
+  document.title = time;
+}
+
+function displayEndTime(then) {
+  const end = new Date(then);
+  const hours = end.getHours();
+  const minutes = end.getMinutes();
+  const time = `Be Back at ${hours < 10 ? "0" : ""}${hours}:${
+    minutes < 10 ? "0" : ""
+  }${minutes}`;
+  endTime.textContent = time;
+}
+
+function startTimer1() {
+  const seconds = this.dataset.seconds;
+  timer(seconds);
+}
+
+function startTimer2(e) {
+  e.preventDefault();
+  const minutes = input.value;
+  const seconds = minutes * 60;
+
+  if (minutes / 1 != minutes) {
+    clearInterval(countdown);
+    remainingTime.textContent = "Please enter only numbers.";
+    remainingTime.style.fontSize = "7rem";
+    document.title = "Countdown Timer";
+    endTime.textContent = "";
+    this.reset();
+    return;
+  }
+
+  if (minutes < 0) {
+    clearInterval(countdown);
+    remainingTime.textContent = "Please enter a positive number.";
+    remainingTime.style.fontSize = "7rem";
+    document.title = "Countdown Timer";
+    endTime.textContent = "";
+    this.reset();
+    return;
+  }
+
+  if (minutes == 0) {
+    clearInterval(countdown);
+    remainingTime.textContent = "Oh, no! Zero?";
+    remainingTime.style.fontSize = "7rem";
+    document.title = "Zero?";
+    endTime.textContent = "";
+    this.reset();
+    return;
+  }
+
+  timer(seconds);
+  this.reset();
+}
+
+buttons.forEach((button) => button.addEventListener("click", startTimer1));
+form.addEventListener("submit", startTimer2);
