52d4017fbf105e00f1ea296d45fa2e3b607a53e6
Nəzrin <isbndyrva@gmail.com>
2022-02-19 05:13:06 +0400

    Add project

---
+++ 12 - Key Sequence Detection/js/script.js
@@ -0,0 +1,16 @@
+const pressed = [];
+const secretWord = "vusale";
+
+window.addEventListener("keyup", (e) => {
+  // console.log(e, e.key);
+  pressed.push(e.key);
+  console.log(pressed);
+
+  pressed.splice(-secretWord.length - 1, pressed.length - secretWord.length);
+  console.log(pressed);
+
+  if (pressed.join("").includes(secretWord)) {
+    console.log("🦄");
+    cornify_add();
+  }
+});
