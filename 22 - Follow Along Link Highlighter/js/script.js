a0594131acc1e0cd877eabb5751133a32ef3cd73
Nəzrin <isbndyrva@gmail.com>
2022-02-22 20:16:19 +0400

    Add script.js

---
+++ 22 - Follow Along Link Highlighter/js/script.js
@@ -0,0 +1,24 @@
+const links = document.querySelectorAll("a");
+const span = document.createElement("span");
+span.classList = "highlight";
+document.body.appendChild(span);
+
+function highlightLink() {
+  const highlightCoords = this.getBoundingClientRect();
+
+  const coords = {
+    width: highlightCoords.width,
+    height: highlightCoords.height,
+    top: highlightCoords.top + window.scrollY,
+    left: highlightCoords.left + window.scrollX,
+  };
+
+  span.style.width = `${coords.width}px`;
+  span.style.height = `${coords.height}px`;
+  span.style.transform = `translate(${coords.left}px, ${coords.top}px`;
+}
+
+links.forEach((link) => link.addEventListener("mouseenter", highlightLink));
+links.forEach((link) =>
+  link.addEventListener("click", (e) => e.preventDefault())
+);
